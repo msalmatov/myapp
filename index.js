69d91c665932607682ba3c9863f0b82d9a47aa67
mroot <almatov.ms@gmail.com>
2021-05-06 14:49:16 +0500

    install cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 require("./components/db");
 const userRoutes = require("./routes/user");
@@ -10,8 +11,11 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// middlewares
+app.use(cors());
 app.use(bodyParser.json());
 
+// rotes
 app.use("/user", userRoutes);
 
 app.get("/", (req, res) => {
